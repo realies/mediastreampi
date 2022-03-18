25fdc71f4537ace98a692450d893a3528b01a94e
realies <5107843+realies@users.noreply.github.com>
2022-03-18 13:24:19 +0200

    fix to oldstable, add ffmpeg, wm5102, gpu config, enable ssh, unauthorise gopro, wpa config

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-export LATEST_IMAGE=$(curl -s https://www.raspberrypi.org/software/operating-systems/ | egrep -o 'https?://[^"]+' | grep armhf-lite.zip$)
+export LATEST_IMAGE=$(curl -s https://www.raspberrypi.com/software/operating-systems/ | egrep -o 'https?://[^"]+' | grep armhf-lite.zip$ | grep -i oldstable)
 cat ./raspios*.json | \
 jq '.builders[].file_urls[] = env.LATEST_IMAGE' | \
 jq '.builders[].file_checksum_url = "\(env.LATEST_IMAGE).sha256"' | \
 jq '.builders[].image_path = "mediastreampi-raspios-arm64.img"' | \
-jq '.builders[].image_size = "2G"' | \
+jq '.builders[].image_size = "3G"' | \
 jq '.builders[].image_build_method = "resize"' | \
-jq '.provisioners[.provisioners| length] += {"type": "shell", "script": "gstreamer.sh"}' > latest.os.json 
+jq '.provisioners[.provisioners| length] += {"type": "shell", "script": "gstreamer.sh"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "shell", "script": "ffmpeg.sh"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "shell", "script": "wm5102.sh"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "shell", "inline": "echo \"start_x=1\ngpu_mem=256\" >> /boot/config.txt"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "shell", "inline": "touch /boot/ssh"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "file", "source": "0-gopro.rules", "destination": "/etc/udev/rules.d/0-gopro.rules"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "file", "source": "wpa_supplicant.conf", "destination": "/boot/wpa_supplicant.conf"}' \
+> latest.os.json
 docker image pull mkaczanowski/packer-builder-arm
 time docker run --rm --privileged -v /dev:/dev -v ${PWD}:/build mkaczanowski/packer-builder-arm build latest.os.json
 rm latest.os.json
+
+# jq '."post-processors" = [{"type": "flasher", "device": "/dev/disk2", "block_size": "4096", "interactive": true }]'
