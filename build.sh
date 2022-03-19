f9033304a4b04876f2c61a1e29930d993a8ef47a
realies <5107843+realies@users.noreply.github.com>
2022-03-19 18:49:22 +0200

    add screen install, fix script ownership and execution

--- build.sh
+++ build.sh
@@ -13,8 +13,11 @@ jq '.provisioners[.provisioners| length] += {"type": "shell", "inline": "echo \"
 jq '.provisioners[.provisioners| length] += {"type": "shell", "inline": "touch /boot/ssh"}' | \
 jq '.provisioners[.provisioners| length] += {"type": "file", "source": "0-gopro.rules", "destination": "/etc/udev/rules.d/0-gopro.rules"}' | \
 jq '.provisioners[.provisioners| length] += {"type": "file", "source": "wpa_supplicant.conf", "destination": "/boot/wpa_supplicant.conf"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "shell", "script": "screen.sh"}' | \
 jq '.provisioners[.provisioners| length] += {"type": "file", "source": "stream.sh", "destination": "/home/pi/stream.sh"}' | \
-jq '.provisioners[.provisioners| length] += {"type": "file", "source": "rc.local", "destination": "/etc/rc.local"}' \
+jq '.provisioners[.provisioners| length] += {"type": "file", "source": "rc.local", "destination": "/etc/rc.local"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "shell", "inline": "chmod +x /home/pi/stream.sh /etc/rc.local"}' | \
+jq '.provisioners[.provisioners| length] += {"type": "shell", "inline": "chown pi:pi /home/pi/stream.sh"}' \
 > latest.os.json
 docker image pull mkaczanowski/packer-builder-arm
 time docker run --rm --privileged -v /dev:/dev -v ${PWD}:/build mkaczanowski/packer-builder-arm build latest.os.json
